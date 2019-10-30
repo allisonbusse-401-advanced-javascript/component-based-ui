4c6eda57bbdb815d7b8d643628db8dcfc4ffe487
allisonbusse <allisonbusse@gmail.com>
2019-10-30 11:32:05 -0700

    readme and component change

---
+++ README.md
@@ -0,0 +1,3 @@
+# Component Based UI
+This lab is the first time creating React components and styling them with external style sheets. It requires displaying 10 characters from the Rick and Morty API with their image, name, and species. 
+
--- src/components/characters/Character.js
+++ src/components/characters/Character.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 const propTypes = require('prop-types');
 import styles from './Character.css';
 
-const Character = ({ img, name, species }) => {
-  return (
-    <li className={styles.Character}>
-      <img src={img} />
-      <div className={styles.info}>
-        <h2>Name: <span className={styles.charName}>{name}</span></h2>
-        <p>Species: {species}</p>
-      </div>
-    </li>
-  );
-};
+class Character extends Component {
+  static propTypes = {
+    img: propTypes.string.isRequired,
+    name: propTypes.string.isRequired,
+    species: propTypes.string.isRequired
+  }
+  render() {
+    const { img, name, species } = this.props;
+    return (
+      <li className={styles.Character}>
+        <img src={img} />
+        <div className={styles.info}>
+          <h2>Name: <span className={styles.charName}>{name}</span></h2>
+          <p>Species: {species}</p>
+        </div>
+      </li>
+    );
+  }
+}
 
-Character.propTypes = {
-  img: propTypes.string.isRequired,
-  name: propTypes.string.isRequired,
-  species: propTypes.string.isRequired
-};
+
 
 
 
